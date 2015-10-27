2adf60d2f7929f4fe4c440d2649846b18fa76e7f
diana <holla@bikefarm.org>
2015-10-26 21:31:15 -0700

    Add behavior for Tweet and User classes

--- app/src/main/java/com/epicodus/twitterclone/models/Tweet.java
+++ app/src/main/java/com/epicodus/twitterclone/models/Tweet.java
@@ -1,7 +1,67 @@
 package com.epicodus.twitterclone.models;
 
+import android.content.Context;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.epicodus.twitterclone.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by admin on 10/26/15.
  */
-public class Tweet {
+
+@Table(name = "Tweets", id = "_id")
+public class Tweet extends Model {
+
+    @Column(name = "Content")
+    private String mContent;
+
+    @Column(name = "CreatedAt")
+    private long mCreatedAt;
+
+    @Column(name = "User")
+    private User mUser;
+
+    public Tweet() {
+        super();
+    }
+
+    public Tweet(String content, User user) {
+        mContent = content;
+        mUser = user;
+        mCreatedAt = new Date().getTime();
+    }
+
+    public String getContent() {
+        return mContent;
+    }
+
+    public void setContent(String content) {
+        mContent = content;
+    }
+
+    public long getCreatedAt() {
+        return mCreatedAt;
+    }
+
+    // Date saved as long bc Java uses the Unix timestamp
+    public void setCreatedAt(long createdAt) {
+        mCreatedAt = createdAt;
+    }
+
+    // make that time more pretty for people eyes
+    public String getFormattedTime(Context context) {
+        SimpleDateFormat formatter = new SimpleDateFormat(context.getString(R.string.formatted_time));
+        formatter.setTimeZone(TimeZone.getTimeZone(context.getString(R.string.timezone)));
+        return formatter.format(mCreatedAt);
+    }
+
+    public User getUser() {
+        return mUser;
+    }
 }
--- app/src/main/java/com/epicodus/twitterclone/models/User.java
+++ app/src/main/java/com/epicodus/twitterclone/models/User.java
@@ -1,12 +1,31 @@
 package com.epicodus.twitterclone.models;
 
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
 /**
  * Created by admin on 10/26/15.
  */
 
+@Table(name = "Users", id="_id")
 
 public class User extends Model {
 
+    @Column(name = "Name")
+    private String mName;
 
+    public User() {
+        super();
+    }
+
+    public User(String name) {
+        super();
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
 
 }
